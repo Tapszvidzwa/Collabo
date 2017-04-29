ec896d73e7217c2ba49f32221a8c45fedafb1515
Tapszvidzwa <zvidzwat@grinnell.edu>
2017-04-29 15:58:11 -0500

    Fix font color

--- app/src/main/java/com/example/tapiwa/collabo/camera.java
+++ app/src/main/java/com/example/tapiwa/collabo/camera.java
@@ -5,21 +5,31 @@ import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 
 public class camera extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     private ImageView Image;
+    private Button captureImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
+        captureImage = (Button) findViewById(R.id.capture);
         Image = (ImageView) findViewById(R.id.imageView);
 
-        takePhoto();
+
+        captureImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePhoto();
+            }
+        });
     }
 
     private void takePhoto() {
