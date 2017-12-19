b6a6a0987ad055e2ca2df0d4c6f8ec512c719c85
Tapszvidzwa <zvidzwat@grinnell.edu>
2017-12-19 16:11:41 -0600

    Code view to stack cards

--- app/src/main/java/com/example/tapiwa/collegebuddy/classContents/notes/NoteStackViewAdapter.java
+++ app/src/main/java/com/example/tapiwa/collegebuddy/classContents/notes/NoteStackViewAdapter.java
@@ -2,18 +2,15 @@ package com.example.tapiwa.collegebuddy.classContents.notes;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
+import android.widget.StackView;
 import android.widget.TextView;
 
 import com.example.tapiwa.collegebuddy.R;
-import com.example.tapiwa.collegebuddy.classContents.images.NewImage;
-import com.squareup.picasso.Callback;
-import com.squareup.picasso.NetworkPolicy;
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -23,11 +20,13 @@ public class NoteStackViewAdapter extends BaseAdapter {
     private ArrayList<NoteStackItem> mData;
     private Context mContext;
     private int layout;
+    private boolean codeModeActivated = false;
 
-    public NoteStackViewAdapter(ArrayList<NoteStackItem> data, int layout, Context context) {
+    public NoteStackViewAdapter(ArrayList<NoteStackItem> data, int layout, Context context, boolean codeMode) {
         this.mData = data;
         this.mContext = context;
         this.layout = layout;
+        this.codeModeActivated = codeMode;
     }
 
     @Override
@@ -74,42 +73,52 @@ public class NoteStackViewAdapter extends BaseAdapter {
 
         final NoteStackItem currentNote = mData.get(position);
         holder.title.setText(currentNote.getTitle());
+        holder.noteContent.setText(currentNote.getContents());
 
-        String color = currentNote.getColor();
+        if(codeModeActivated) {
 
-        if(color != null) {
+            holder.noteContent.setBackgroundColor(Color.GRAY);
 
-            switch (color) {
-                case "red":
-                    holder.title.setBackgroundColor(Color.RED);
-                    break;
-                case "green":
-                    holder.title.setBackgroundColor(Color.GREEN);
-                    break;
-                case "magenta":
-                    holder.title.setBackgroundColor(Color.MAGENTA);
-                    break;
-                case "black":
-                    holder.title.setBackgroundColor(Color.BLACK);
-                    holder.title.setTextColor(Color.WHITE);
-                    break;
-                case "white":
-                    holder.title.setBackgroundColor(Color.WHITE);
-                    break;
-                case "yellow":
-                    holder.title.setBackgroundColor(Color.YELLOW);
-                    break;
-                case "blue":
-                    holder.title.setBackgroundColor(Color.BLUE);
-                    holder.title.setTextColor(Color.WHITE);
-                    break;
-                case "cyan":
-                    holder.title.setBackgroundColor(Color.CYAN);
-                    break;
+            Typeface typeface = Typeface.createFromAsset(mContext.getAssets(), "fonts/sourcecodeproregular.ttf");
+            holder.noteContent.setTypeface(typeface);
+            holder.noteContent.setTextColor(Color.WHITE);
+        } else {
+
+
+            String color = currentNote.getColor();
+
+            if (color != null) {
+
+                switch (color) {
+                    case "red":
+                        holder.title.setBackgroundColor(Color.RED);
+                        break;
+                    case "green":
+                        holder.title.setBackgroundColor(Color.GREEN);
+                        break;
+                    case "magenta":
+                        holder.title.setBackgroundColor(Color.MAGENTA);
+                        break;
+                    case "black":
+                        holder.title.setBackgroundColor(Color.BLACK);
+                        holder.title.setTextColor(Color.WHITE);
+                        break;
+                    case "white":
+                        holder.title.setBackgroundColor(Color.WHITE);
+                        break;
+                    case "yellow":
+                        holder.title.setBackgroundColor(Color.YELLOW);
+                        break;
+                    case "blue":
+                        holder.title.setBackgroundColor(Color.BLUE);
+                        holder.title.setTextColor(Color.WHITE);
+                        break;
+                    case "cyan":
+                        holder.title.setBackgroundColor(Color.CYAN);
+                        break;
+                }
             }
         }
-
-        holder.noteContent.setText(currentNote.getContents());
 
 
         return row;
--- app/src/main/java/com/example/tapiwa/collegebuddy/classContents/notes/StackCards.java
+++ app/src/main/java/com/example/tapiwa/collegebuddy/classContents/notes/StackCards.java
@@ -5,17 +5,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.StackView;
+import android.widget.Toast;
 
 import com.example.tapiwa.collegebuddy.Analytics.AppUsageAnalytics;
 import com.example.tapiwa.collegebuddy.R;
+import com.example.tapiwa.collegebuddy.classContents.classContentsMain.ClassContentsMainActivity;
+import com.example.tapiwa.collegebuddy.classContents.notes.SelectUsers.SelectUsers;
+import com.example.tapiwa.collegebuddy.miscellaneous.GenericServices;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 
 import java.util.ArrayList;
 import java.util.Collections;
+
+import es.dmoral.toasty.Toasty;
 
 
 public class StackCards extends AppCompatActivity {
@@ -26,6 +34,7 @@ public class StackCards extends AppCompatActivity {
     private StackView stackView;
     private Toolbar mToolbar;
     private ArrayList<NoteStackItem> noteCards;
+    private boolean codeMode = false;
 
 
 
@@ -50,7 +59,7 @@ public class StackCards extends AppCompatActivity {
         listCards = new ArrayList<>();
         stackView = (StackView) findViewById(R.id.swipeCardsStack);
         mToolbar = (Toolbar) findViewById(R.id.swipeCardsToolbar);
-        mToolbar.setTitle("Cards");
+        mToolbar.setTitle(ClassContentsMainActivity.className + " Notes");
 
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -60,16 +69,11 @@ public class StackCards extends AppCompatActivity {
         Bundle args = intent.getBundleExtra("BUNDLE");
         noteCards = (ArrayList<NoteStackItem>) args.getSerializable("ARRAYLIST");
 
-        //swipeStackAdapter = new SwipeStackAdapter(listCards, StackCardsImages.this);
-       // stackView.setAdapter(swipeStackAdapter);
-
+        stackViewAdapter = new NoteStackViewAdapter(noteCards, R.layout.stack_note_item, StackCards.this, false);
+        stackView.setAdapter(stackViewAdapter);
     }
 
     private void initializeListeners() {
-
-               // Collections.reverse(noteCards);
-                stackViewAdapter = new NoteStackViewAdapter(noteCards, R.layout.stack_note_item, StackCards.this);
-                stackView.setAdapter(stackViewAdapter);
 
                 stackView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -81,19 +85,44 @@ public class StackCards extends AppCompatActivity {
                 startActivity(openDisplayNote);
             }
         });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.stack_cards_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Main/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //// TODO: 8/1/17 Change these settings to custom settings
+        if (id == R.id.stack_cards_new_note_mode) {
+            if(!codeMode) {
+                codeMode = true;
+                stackViewAdapter = new NoteStackViewAdapter(noteCards, R.layout.stack_note_item, StackCards.this, codeMode);
+                Toasty.info(getApplicationContext(), getString(R.string.code_mode_activate), Toast.LENGTH_SHORT).show();
+
+            } else {
+                codeMode = false;
+                stackViewAdapter = new NoteStackViewAdapter(noteCards, R.layout.stack_note_item, StackCards.this, codeMode);
+                Toasty.info(getApplicationContext(), getString(R.string.code_mode_deactivate), Toast.LENGTH_SHORT).show();
             }
 
 
+            stackView.setAdapter(stackViewAdapter);
+            stackViewAdapter.notifyDataSetChanged();
 
+        }
 
-
-
-
-
-
-
-
-
+        return super.onOptionsItemSelected(item);
+    }
 
 
     @Override
